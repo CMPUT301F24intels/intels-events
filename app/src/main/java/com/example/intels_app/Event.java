35730ae454d2dbdc3a205081171b297a66d0d72a
Janan Panchal <jpanchal@ualberta.ca>
2024-10-23 22:29:55 -0600

    Initialized variables for Event class

--- app/src/main/java/com/example/intels_app/Event.java
+++ app/src/main/java/com/example/intels_app/Event.java
@@ -1,4 +1,17 @@
 package com.example.intels_app;
 
 public class Event {
+    private String eventName;
+    private String facilityName;
+    private String location;
+    private String date;
+    private String time;
+    private String description;
+    private int maxAttendees;
+    private boolean geolocationRequirement;
+    private boolean notifPreference;
+
+    // Add pfp
+    // https://developer.android.com/training/data-storage/shared
+
 }
