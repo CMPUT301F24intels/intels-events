2abd92c55172c9dbfd92449e7f1d01e7293532ae
Aayushi Shah <aayushishah@Aayushis-Air.lan>
2024-10-28 16:16:41 -0600

    added the entrant/organizer view my waitlist page

--- app/src/main/java/com/example/intels_app/MainActivity.java
+++ app/src/main/java/com/example/intels_app/MainActivity.java
@@ -3,43 +3,46 @@ package com.example.intels_app;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.intels_app.databinding.ActivityMainBinding;
-import com.google.firebase.firestore.FirebaseFirestore;
+//import com.example.intels_app.databinding.ActivityMainBinding;
+//import com.google.firebase.firestore.FirebaseFirestore;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+//import com.google.zxing.integration.android.IntentIntegrator;
+//import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseFirestore db;
+    //private FirebaseFirestore db; *error out for me so commented out for now
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    //private ActivityMainBinding binding; *error out for me so commented out for now
     private QRCodeScanner qrCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        db = FirebaseFirestore.getInstance();
+        //db = FirebaseFirestore.getInstance(); *error out for me so commented out for now
 
         setContentView(R.layout.main_page);
         qrCodeScanner = new QRCodeScanner(this);
 
         // Change it to qr code scanner button ion UI later
         //qrCodeScanner.startScan();
+        ImageButton ViewWaitListButton = findViewById(R.id.imageButton7);
+        ViewWaitListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EventGridActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
