598f3332a60744b5ccf30ea47fca5004bd000616
Janan Panchal <jpanchal@ualberta.ca>
2024-10-29 20:29:29 -0600

    Connecting main page Manage Events button to associated layout with functioning back button

--- app/src/main/java/com/example/intels_app/MainActivity.java
+++ app/src/main/java/com/example/intels_app/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import android.view.MenuInflater;
 import android.view.View;
