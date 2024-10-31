ac9b64e739f06115bb8f0a58800b87b11194669a
Janan Panchal <jpanchal@ualberta.ca>
2024-10-31 16:13:24 -0600

    Create UI for dispalying QR Code after Event creation

---
+++ app/src/main/java/com/example/intels_app/CreateQR.java
@@ -0,0 +1,15 @@
+package com.example.intels_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_code_display);
+    }
+}
