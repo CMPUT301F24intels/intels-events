20b41537e71afdb06d0de0becf9bf199ac6cca4b
Janan Panchal <jpanchal@ualberta.ca>
2024-11-05 15:13:02 -0700

    Display events from Firestore for Admin View

--- app/src/main/java/com/example/intels_app/AdminEvents.java
+++ app/src/main/java/com/example/intels_app/AdminEvents.java
@@ -2,6 +2,7 @@ package com.example.intels_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
@@ -9,6 +10,10 @@ import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,13 +40,32 @@ public class AdminEvents extends AppCompatActivity {
 
         events_gridview = findViewById(R.id.events_gridview);
         list_event = new ArrayList<>();
-        list_event.add(new Event("1", "Event 1"));
-        list_event.add(new Event("2", "Event 2"));
-        list_event.add(new Event("3", "Event 3"));
-        list_event.add(new Event("4", "Event 4"));
-        list_event.add(new Event("5", "Event 5"));
+        //list_event.add(new Event("1", "Event 1"));
+        //list_event.add(new Event("2", "Event 2"));
+        //list_event.add(new Event("3", "Event 3"));
+        //list_event.add(new Event("4", "Event 4"));
+        //list_event.add(new Event("5", "Event 5"));
         CustomAdapterOrganizer adapter = new CustomAdapterOrganizer(this, list_event);
         events_gridview.setAdapter(adapter);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionRef = db.collection("events"); // Replace "events" with your collection name
+
+        collectionRef.get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (!queryDocumentSnapshots.isEmpty()) {
+                        for (DocumentSnapshot document : queryDocumentSnapshots) {
+
+                            // Convert document to Event object and add to eventData list
+                            Event event = document.toObject(Event.class);
+                            list_event.add(event);
+                            adapter.notifyDataSetChanged();
+                        }
+                    } else {
+                        Log.d("Firestore", "No documents found in this collection.");
+                    }
+                })
+                .addOnFailureListener(e -> Log.w("Firestore", "Error fetching documents", e));
 
         profile_button = findViewById(R.id.profile_button);
         events_button = findViewById(R.id.events_button);
