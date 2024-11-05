5fb644856693bd9002d80ddf62aa1c15ec28cd9f
Janan Panchal <jpanchal@ualberta.ca>
2024-11-04 17:40:26 -0700

    Attempting with Firebase

--- app/src/main/java/com/example/intels_app/ManageEventsActivity.java
+++ app/src/main/java/com/example/intels_app/ManageEventsActivity.java
@@ -1,7 +1,10 @@
 package com.example.intels_app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +14,10 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
