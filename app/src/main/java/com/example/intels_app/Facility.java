52384a6e90a0abcb3eed7011276b27c38eb56b5f
Janan Panchal <jpanchal@ualberta.ca>
2024-11-05 20:53:29 -0700

    Add manage facility xml and class

---
+++ app/src/main/java/com/example/intels_app/Facility.java
@@ -0,0 +1,61 @@
+package com.example.intels_app;
+
+public class Facility {
+    private String facilityName;
+    private String location;
+    private String organizerName;
+    private String email;
+    private int telephone;
+    private String posterUrl;
+
+    // No argument constructor for Firebase
+    public Facility() {}
+
+    public String getFacilityName() {
+        return facilityName;
+    }
+
+    public void setFacilityName(String facilityName) {
+        this.facilityName = facilityName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOrganizerName() {
+        return organizerName;
+    }
+
+    public void setOrganizerName(String organizerName) {
+        this.organizerName = organizerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+}
