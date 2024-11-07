202ee59ff4ac5f93d665fd36fd666781af3109a6
Janan Panchal <jpanchal@ualberta.ca>
2024-11-06 23:54:26 -0700

    In Admin view profiles, fixed the issue of profiles not showing up - display pfp as well

--- app/src/main/java/com/example/intels_app/AdminProfiles.java
+++ app/src/main/java/com/example/intels_app/AdminProfiles.java
@@ -44,15 +44,10 @@ public class AdminProfiles extends AppCompatActivity {
                         for (DocumentSnapshot document : queryDocumentSnapshots) {
 
                             // Convert document to Profile object and add to profileList
-                            //Profile profile = document.toObject(Profile.class);
-                            //Log.d("Firestore", "Name: " + profile.getName() + "ID: " + profile.getImageResId());
-                            //Log.d("Firestore", "Name: " + profile.getName() + "ID: " + profile.getImageResId());
-
-                            Profile setProfile = new Profile(document.getString("name"), document.getString("email"));
-
+                            Profile profile = document.toObject(Profile.class);
                             Log.d("Firestore", "name: " + document.getString("name"));
 
-                            profileList.add(setProfile);
+                            profileList.add(profile);
                             adapter.notifyDataSetChanged();
                         }
                     } else {
