99db8d5ed8293b49928bea5d504e655b60f3cf33
het-patell <hetbhave@ualberta.ca>
2024-12-02 09:51:50 -0800

    Added zoom feature

--- app/src/main/java/com/example/intels_app/MapsActivity.java
+++ app/src/main/java/com/example/intels_app/MapsActivity.java
@@ -101,6 +101,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
+        // Enable zoom controls and gestures
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        // Set minimum and maximum zoom levels
+        mMap.setMinZoomPreference(2.0f);
+        mMap.setMaxZoomPreference(21.0f);
 
         // Enable My Location layer if permission is granted
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
